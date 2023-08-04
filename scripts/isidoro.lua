57470494d155316f76ccd5b483d5b951363640e0
An-Annoying-Cat <57719091+An-Annoying-Cat@users.noreply.github.com>
2023-08-04 01:19:47 -0500

    added taunt

--- scripts/isidoro.lua
+++ scripts/isidoro.lua
@@ -1,9 +1,11 @@
 local Mod = Isidoro
 Mod.ISIDORO = {}
 local ISIDORO = Mod.ISIDORO
-
 ISIDORO.PLAYER_ISIDORO = Isaac.GetPlayerTypeByName("Isidoro")
 
+local uninterruptable_actions = {
+
+}
 
 -- assign costume
 function ISIDORO:IsidoroInit(player)
@@ -12,17 +14,52 @@ function ISIDORO:IsidoroInit(player)
     local sprite = player:GetSprite()
     sprite:Load("gfx/characters/isidoro.anm2", true)
 end
-ISIDORO:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, ISIDORO.IsidoroInit)
+Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, ISIDORO.IsidoroInit)
 
 
 function ISIDORO:IsidoroUpdate(player)
     if player:GetPlayerType() ~= ISIDORO.PLAYER_ISIDORO then return end
 
     local sprite = player:GetSprite()
-    if player:GetMovementVector():Length() < 0.1 and not sprite:IsPlaying("IdleNormal") and player:IsExtraAnimationFinished() then
+    local data = player:GetData()
+
+
+    if player:GetMovementVector():Length() == 0 and not sprite:IsPlaying("IdleNormal") and player:IsExtraAnimationFinished() then
         player:PlayExtraAnimation("IdleNormal")
-    elseif player:GetMovementVector():Length() > 0.1 and sprite:IsPlaying("IdleNormal") then
+        sprite:RemoveOverlay()
+        player.ControlsEnabled = true
+    elseif player:GetMovementVector():Length() > 0 and sprite:IsPlaying("IdleNormal") then
         player:StopExtraAnimation()
     end
+
+    if sprite:IsPlaying("TauntFX") then
+        if data.isi_overlay then
+            data.isi_overlay:Render(Isaac.WorldToRenderPosition(player.Position))
+        end
+    end
+
 end
-ISIDORO:AddCallback(ModCallbacks.MC_POST_PLAYER_RENDER, ISIDORO.IsidoroUpdate)
+Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_RENDER, ISIDORO.IsidoroUpdate, 0)
+
+
+-----PARRY-----
+
+function ISIDORO:Parry(player)
+    if player:GetPlayerType() ~= ISIDORO.PLAYER_ISIDORO then return end
+
+    local sprite = player:GetSprite()
+    local rng = player:GetDropRNG()
+    local data = player:GetData()
+
+    if Input.IsActionTriggered(ButtonAction.ACTION_DROP, player.ControllerIndex) then
+        player:PlayExtraAnimation("TauntFX", true)
+
+        local overlay = Sprite() --this game SUCKS so overlay animations dont work with player sprites so i have to do this instead
+        overlay:Load("gfx/characters/isidoro.anm2", true)
+        overlay:SetFrame("Taunt", rng:RandomInt(16))
+        data.isi_overlay = overlay
+
+        player.ControlsEnabled = false
+    end
+end
+Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, ISIDORO.Parry, 0)
