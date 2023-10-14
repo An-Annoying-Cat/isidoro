58f36db9db7567ccac8913ba514304b6d45a66ad
An-Annoying-Cat <57719091+An-Annoying-Cat@users.noreply.github.com>
2023-10-14 01:29:06 -0500

    parry added

--- scripts/isidoro.lua
+++ scripts/isidoro.lua
@@ -2,10 +2,42 @@ local Mod = Isidoro
 Mod.ISIDORO = {}
 local ISIDORO = Mod.ISIDORO
 ISIDORO.PLAYER_ISIDORO = Isaac.GetPlayerTypeByName("Isidoro")
+-- Mod.ENUMS.SOUNDS.TAUNT = Isaac.GetSoundIdByName("Isidoro Taunt") ill get to setting up enums later
+-- Mod.ENUMS.SOUNDS.PARRY = Isaac.GetSoundIdByName("Isidoro Parry")
+local TAUNTSOUND = Isaac.GetSoundIdByName("Isidoro Taunt")
+local PARRYSOUND = Isaac.GetSoundIdByName("Isidoro Parry")
+
+ISIDORO.PARRY_WINDOW = 12
+
+local PARRY_WINDOW = ISIDORO.PARRY_WINDOW
 
 local uninterruptable_actions = {
 
 }
+
+function ISIDORO:GetIsidoroState(player)
+    if not player:GetData().isi_data then
+		player:GetData().isi_data = {
+			TauntTime = 0
+		}
+	end
+	return player:GetData().isi_data
+end
+
+function ISIDORO:IsTaunting(player)
+    local sprite = player:GetSprite()
+    return sprite:IsPlaying("TauntFX")
+end
+
+function ISIDORO:ResetState(player)
+    local isiData = ISIDORO:GetIsidoroState(player)
+
+    player.ControlsEnabled = true
+    isiData.TauntTime = 0
+    player:ResetDamageCooldown()
+end
+
+
 
 -- assign costume
 function ISIDORO:IsidoroInit(player)
@@ -13,6 +45,7 @@ function ISIDORO:IsidoroInit(player)
 
     local sprite = player:GetSprite()
     sprite:Load("gfx/characters/isidoro.anm2", true)
+    ISIDORO:ResetState(player)
 end
 Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, ISIDORO.IsidoroInit)
 
@@ -22,12 +55,12 @@ function ISIDORO:IsidoroUpdate(player)
 
     local sprite = player:GetSprite()
     local data = player:GetData()
+    -- local isiData = ISIDORO:GetIsidoroState(player)
 
 
     if player:GetMovementVector():Length() == 0 and not sprite:IsPlaying("IdleNormal") and player:IsExtraAnimationFinished() then
+        ISIDORO:ResetState(player)
         player:PlayExtraAnimation("IdleNormal")
-        sprite:RemoveOverlay()
-        player.ControlsEnabled = true
     elseif player:GetMovementVector():Length() > 0 and sprite:IsPlaying("IdleNormal") then
         player:StopExtraAnimation()
     end
@@ -44,15 +77,18 @@ Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_RENDER, ISIDORO.IsidoroUpdate, 0)
 
 -----PARRY-----
 
-function ISIDORO:Parry(player)
+function ISIDORO:Taunt(player)
     if player:GetPlayerType() ~= ISIDORO.PLAYER_ISIDORO then return end
 
     local sprite = player:GetSprite()
     local rng = player:GetDropRNG()
     local data = player:GetData()
+    local isiData = ISIDORO:GetIsidoroState(player)
 
     if Input.IsActionTriggered(ButtonAction.ACTION_DROP, player.ControllerIndex) then
         player:PlayExtraAnimation("TauntFX", true)
+        SFXManager():Play(TAUNTSOUND)
+
 
         local overlay = Sprite() --this game SUCKS so overlay animations dont work with player sprites so i have to do this instead
         overlay:Load("gfx/characters/isidoro.anm2", true)
@@ -61,5 +97,45 @@ function ISIDORO:Parry(player)
 
         player.ControlsEnabled = false
     end
+
+    if isiData.TauntTime > 0 then
+        player.ControlsEnabled = false
+        isiData.TauntTime = isiData.TauntTime + 1
+    end
 end
-Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, ISIDORO.Parry, 0)
+Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, ISIDORO.Taunt, 0)
+
+
+function ISIDORO:DeflectProjectile(player, projectile)
+	local playerPos = player.Position
+    local projectilePos = projectile.Position
+
+    projectile:ToProjectile():AddProjectileFlags(ProjectileFlags.HIT_ENEMIES)
+    projectile.Velocity = projectile.Velocity * -1.5
+
+	-- if math.abs(projectilePos.X - playerPos.X) > math.abs(projectilePos.Y - playerPos.Y) then
+    --     projectile.Velocity = Vector(-projectile.Velocity.X, projectile.Velocity.Y)
+	-- else
+    --     projectile.Velocity = Vector(projectile.Velocity.X, -projectile.Velocity.Y)
+	-- end
+end
+
+
+function ISIDORO:ParryProjectile(projectile, collider)
+
+    local player = collider:ToPlayer()
+    local isiData = ISIDORO:GetIsidoroState(player)
+
+    if ISIDORO:IsTaunting(player) and isiData.TauntTime <= PARRY_WINDOW then
+        player:PlayExtraAnimation("Parry", true)
+        player:SetMinDamageCooldown(20)
+        SFXManager():Play(PARRYSOUND)
+
+        for _, bullet in pairs(Isaac.FindInRadius(player.Position, 28, EntityPartition.BULLET)) do
+			ISIDORO:DeflectProjectile(player, bullet)
+		end
+
+        return true
+    end
+end
+Mod:AddCallback(ModCallbacks.MC_PRE_PROJECTILE_COLLISION, ISIDORO.ParryProjectile)
